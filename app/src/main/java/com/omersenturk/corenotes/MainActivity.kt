940c61cd61f5f78ef1ed382d7755164c0c59f116
Omer Senturk <omersenturk.dev@gmail.com>
2026-01-03 20:11:42 +0300

    fix

--- app/src/main/java/com/omersenturk/corenotes/MainActivity.kt
+++ app/src/main/java/com/omersenturk/corenotes/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.omersenturk.corenotes
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
